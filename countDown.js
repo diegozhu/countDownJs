f4f0ff49060e343e404431e04940b02132e641f5
diegozhu <diego@diegozhu.net>
2016-05-08 20:22:59 +0800

    Update countDown.js

--- countDown.js
+++ countDown.js
@@ -13,15 +13,45 @@ function countDown(beginFunc,endFunc,intervalFunc,totalCount,timespanMillisecond
 				_endFunc : endFunc,
 				_totalCount : totalCount,
 				_timespanMillisecond : timespanMillisecond,
-				_currentCount : 0,
+				_currentCount : -1,
+				finished : false,
 				clear : function(){
 					for(var i in this.handlers){
 						clearTimeout(this.handlers[i]);
 					}
+					this.handlers = [];
+					return this;
+				},
+				pause : function(){
+					return this.clear();
+				},
+				resume : function(){
+					if(this.finished)
+						return this;
+					if(this.handlers.length > 0){
+						console.error("not paused");
+						return this;
+					}
+					for(var i = this._currentCount+1;i<this._totalCount;i++){
+						(function(time){
+							var self = this;
+							var a = setTimeout(function(){
+								intervalFunc.call(self,time,self._totalCount,self._timespanMillisecond)
+								self._currentCount = time;
+							},timespanMillisecond*(time-this._currentCount-1));
+							this.handlers.push(a);
+						}).call(this,i);
+					}
+					var a = setTimeout(function(){
+						endFunc(totalCount,totalCount,timespanMillisecond);
+					},timespanMillisecond * (this._totalCount-this._currentCount));
+					this.handlers.push(a);
+					return this;
 				},
 				interrupt : function(){
 					this.clear();
 					this._endFunc(this._currentCount,this._totalCount,this._timespanMillisecond);
+					return this;
 				}
 		};
 		for(var i = 0;i<totalCount;i++){
@@ -37,6 +67,7 @@ function countDown(beginFunc,endFunc,intervalFunc,totalCount,timespanMillisecond
 			beginFunc.call(handler,-1,totalCount,timespanMillisecond);
 		},0);
 		var a = setTimeout(function(){
+			handler.finished = true;
 			endFunc(totalCount,totalCount,timespanMillisecond);
 		},timespanMillisecond * totalCount);
 		handler.handlers.push(a);
